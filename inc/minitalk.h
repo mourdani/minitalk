91c76dc70d22faa1175556ffdf8819d355e6d63e
mourdani <mourdani@student.42.fr>
2022-02-10 05:38:58 +0100

    fix norm .h files

--- inc/minitalk.h
+++ inc/minitalk.h
@@ -6,17 +6,17 @@
 /*   By: mourdani <mourdani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 13:28:10 by mourdani          #+#    #+#             */
-/*   Updated: 2022/02/07 00:01:04 by mourdani         ###   ########.fr       */
+/*   Updated: 2022/02/10 05:37:30 by mourdani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define  MINTALK_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <errno.h>
-#include "../libft/includes/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <errno.h>
+# include "../libft/includes/libft.h"
 
 #endif
